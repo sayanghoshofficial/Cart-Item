35bde0ec2a52dc202755aedb9953d750898ccf17
Sayan Ghosh <sayanghosh379@gmail.com>
2023-02-27 18:22:21 +0530

    Add Cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import CartItem from './CartItem';
 
+import Cart from './Cart';
 
 function App() {
   return (
-    <div className="App">
-     
-      <CartItem/>
+    <div className="cart">
+      <Cart/>
+      <Cart/>
+      <Cart/>
     </div>
   );
 }
---
+++ src/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItem from './CartItem';
+class Cart extends React.Component{
+ render(){
+    return(
+        <CartItem/>
+    )
+ }
+}
+
+
+export default Cart;
