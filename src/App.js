f7da81a165d1e938ef1941f463d96bc4757c92af
Sayan Ghosh <sayanghosh379@gmail.com>
2023-03-01 18:59:25 +0530

    setting up firebase on 8.10.0 version

--- src/App.js
+++ src/App.js
@@ -1,189 +1,121 @@
 import React from "react";
-import Cart from './Cart';
+import "./index.css";
+// import CartItem from './CartItem';
+import Cart from "./Cart";
 import Navbar from "./Navbar";
-import {
-  doc,
-  addDoc,
-  setDoc,
-  collection,
-  updateDoc,
-  deleteDoc,
-  onSnapshot,
-  query,
-  where,
-  orderBy,
-  // doc,
-} from "firebase/firestore";
-import { db } from "./index";
-
-
+// import * as firebase from 'firebase';
 class App extends React.Component {
-  
-  constructor () {
+  constructor() {
     super();
     this.state = {
-        products:[
-            // {
-            //     price: 9999,
-            //     title: 'Mobile Phone',
-            //     Qty: 5,
-            //     img: 'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=367&q=80',
-            //     id:1
-            // },
-            // {
-            //     price: 99999,
-            //     title: 'Laptop',
-            //     Qty: 1,
-            //     img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
-            //     id:2
-            // },
-            // {
-            //     price: 999,
-            //     title: 'Watch',
-            //     Qty: 10,
-            //     img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
-            //     id:3
-            // },
-            // {
-            //   price: 999,
-            //   title: 'Perfume',
-            //   Qty: 3,
-            //   img: 'https://images.unsplash.com/photo-1541643600914-78b084683601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=404&q=80',
-            //   id: 4
-            // }
-        ]
-    }
-}
-//function to change 
-async componentDidMount() {
-  //this is a realtime listener if you change anything in firebase ui will automatically updated 
-     const q = query(
-       collection(db, "products"),
-       where("price", ">", 0),
-       orderBy("price")
-     );
-     const unsub = await onSnapshot(q, (querySnapshot) => {
-       const getProducts = [];
-       querySnapshot.forEach((doc) => {
-         const product = doc.data();
-         product.id = doc.id;
-         getProducts.push(product);
-       });
-       console.log(getProducts);
-       this.setState({ products: getProducts, loading: false });
-     });
-   }
-   //changed increase qty
+      products: [
+        {
+          Qty: 10,
+          price: 8999,
+          title: "Mobile phone",
+          img:
+            "https://www.91-img.com/pictures/132721-v10-vivo-v15-pro-mobile-phone-large-1.jpg?tr=h-330,q-75",
+          id: 1
+        },
+        {
+          Qty: 1,
+          price: 77777,
+          title: "Washing Machine",
+          img:
+            "https://images-na.ssl-images-amazon.com/images/I/81HApTZ8D8L._SL1500_.jpg",
+          id: 2
+        },
+        {
+          Qty: 4,
+          price: 999,
+          title: "Watch",
+          img:
+            "https://staticimg.titan.co.in/Titan/Catalog/1806SL03_1.jpg?pView=pdp",
+          id: 3
+        }
+      ]
+    };
+  }
 
-  handleOnIncreaseQty = async (product) => {
-    let { products } = this.state;
+  handleIncreaseQuantity = product => {
+    const { products } = this.state;
     const index = products.indexOf(product);
 
-    const docRef = doc(collection(db, "products"), products[index].id);
-    await updateDoc(docRef, {
-      Qty: products[index].Qty + 1,
+    products[index].Qty += 1;
+
+    this.setState({
+      products
     });
   };
-// handledIncreaseQuantity= (product) =>{
-//     // console.log('hey please increase the quantity of ',product);
-//     const {products} = this.state;
-//     const index = products.indexOf(product);
 
-//     products[index].Qty += 1;
+  handleDecreaseQuantity = product => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
 
-//     this.setState({
-//         products
-//     })
-// }
-//delete from the database
-handleOnDeleteProduct = (productToDelete) => {
-  const docRef = doc(collection(db, "products"), productToDelete);
-  deleteDoc(docRef)
-    .then(() => {
-      console.log("product deleted");
-    })
-    .catch((error) => {
-      console.log(error);
+    if (products[index].Qty === 0) {
+      return;
+    }
+    products[index].Qty -= 1;
+
+    this.setState({
+      products
     });
-};
-handleOnDecreaseQty = async (product) => {
-  let { products } = this.state;
-  const index = products.indexOf(product);
-  if (products[index].Qty !== 0) {
-    const docRef = doc(collection(db, "products"), products[index].id);
-    await updateDoc(docRef, {
-      Qty: products[index].Qty - 1,
+  };
+
+  handleDeleteProduct = id => {
+    const { products } = this.state;
+
+    const items = products.filter(product => product.id !== id);
+
+    this.setState({
+      products: items
     });
-  }
-};
+  };
 
-// handledDecreaseQuantity = (product) =>{
-    
-//     const {products} = this.state;
-//     const index = products.indexOf(product);
-//     // console.log('hey please increase the quantity ofxx ',products[index].Qty);
-//     if(products[index].Qty === 0){
-//         return;
-//     }
-//     products[index].Qty -= 1;
-
-//     this.setState({
-//         products
-//     })
-//   }
-  // handledDeleteProduct = (id) => {
-  //   const {products} = this.state;
-
-  //   const items = products.filter((item) => item.id != id);
-
-  //   this.setState({
-  //       products:  items
-  //   })
-  // }
-  getCartCount = () =>{
-    const {products} = this.state;
-
+  getcountOfCartItems = () => {
+    const { products } = this.state;
     let count = 0;
 
-    products.forEach((product) =>{
+    products.forEach(product => {
       count += product.Qty;
-    })
-
+    });
 
     return count;
-  }
-  getCartTotal = () =>{
-    const {products} = this.state;
+  };
 
+  getcartTotal = () => {
+    const { products } = this.state;
     let cartTotal = 0;
 
-    products.map((product) =>{
-      cartTotal += product.Qty * product.price;
+    products.map(product => {
+      if (product.Qty > 0) {
+        cartTotal = cartTotal + product.Qty * product.price;
+      }
+      return "";
     });
+
     return cartTotal;
-  }
-  render(){
-    const {products} = this.state;
+  };
+
+  render() {
+    const { products } = this.state;
     return (
       <div className="App">
-        <Navbar
-          count = {this.getCartCount()}
-        />
+        <Navbar count={this.getcountOfCartItems()} />
         <Cart
-          products = {products}
-          onIncreasQuantity = {this.handleOnIncreaseQty}
-          onDecreasQuantity = {this.handleOnDecreaseQty}
-          onDeleteProduct = {this.handleOnDeleteProduct}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+          products={products}
         />
-         <div style={{fontSize:20,padding:10}}>
-            Total: {this.getCartTotal()}
+        <div style={{ padding: 10, fontSize: 20 }}>
+          TOTAL : {this.getcartTotal()}
         </div>
       </div>
-     
     );
   }
 }
 
-
-
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase/app";
+import "firebase/firestore";
 //do import these and remove old firebase imports
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDA4ZaBzYXCO5O_8883gUGq_YQw23WyShQ",
   authDomain: "cart-1a59d.firebaseapp.com",
+  databaseURL: "https://cart-1a59d.firebaseio.com",
   projectId: "cart-1a59d",
   storageBucket: "cart-1a59d.appspot.com",
   messagingSenderId: "815455636799",
   appId: "1:815455636799:web:0f2f1fdff844269065578a"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); //create instance of firebase access and export it 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 
@@ -30,5 +33,7 @@ root.render(
   //   <App />
   // </React.StrictMode>
 );
-
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
